2fd688c0ef18aa3e31742033e705c48a65ca031b
RitupaSaha <30826678+RitupaSaha@users.noreply.github.com>
2023-02-02 02:48:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAnGFEWM0RToHityKdxzPFGS3SwyQw68ZU",
-    authDomain: "spectogram-e37dd.firebaseapp.com",
-    databaseURL: "https://spectogram-e37dd-default-rtdb.firebaseio.com",
-    projectId: "spectogram-e37dd",
-    storageBucket: "spectogram-e37dd.appspot.com",
-    messagingSenderId: "43218892872",
-    appId: "1:43218892872:web:9bed0f6ab2aa31b3bd807d"
-  };
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyDU5oVv6G6_h_2e4-O35AU6Ef5Mqfi-phU",
+  authDomain: "story-challenge-83d3a.firebaseapp.com",
+  databaseURL: "https://story-challenge-83d3a-default-rtdb.firebaseio.com",
+  projectId: "story-challenge-83d3a",
+  storageBucket: "story-challenge-83d3a.appspot.com",
+  messagingSenderId: "738847877241",
+  appId: "1:738847877241:web:5e38c59c67575fabefcca8"
+};
